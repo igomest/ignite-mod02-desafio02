3c7a8b60f981bc5ce2807a08f4f6db3e31ab50fd
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-12 14:38:09 -0300

    finalizing application

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,23 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  Icon?: any;
+}
+
+
+const Input = ({ name, Icon, ...rest }: InputProps) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
